d4f04d541f9e2e3ea375d906222784e08d2c6537
Natalie Clarius <natalie_clarius@yahoo.de>
2022-05-24 17:43:21 +0200

    initial release

--- debug_off.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-name=$(grep -oP '(?<=X-KDE-PluginInfo-Name=).*' ./metadata.desktop)
-kwriteconfig5 --file kwinrc --group Script-"$name" --key debugMode false
-qdbus org.kde.KWin /KWin reconfigure
--- disable.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-name=$(grep -oP '(?<=X-KDE-PluginInfo-Name=).*' ./metadata.desktop)
-kwriteconfig5 --file kwinrc --group Plugins --key "$name"Enabled false
-qdbus org.kde.KWin /KWin reconfigure
